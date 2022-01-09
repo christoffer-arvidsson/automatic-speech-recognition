9f8976e6b6e55787856e2b3546b1d39b17264ab7
Christoffer Arvidsson <christoffer@arvidson.nu>
2022-01-09 14:37:10 +0100

    Add evaluation script for CER

---
+++ src/evaluate.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_io as tfio
+import matplotlib.pyplot as plt
+
+from data import create_dataset
+from model import EndToEnd
+from callbacks import DisplaySentence, checkpoint_cb
+from metrics import CERMetric
+from jiwer import cer
+
+batch_size = 64
+patch_width = 32
+lr = 2e-4
+
+train_metadata_path = '../dataset/sv-SE/validated.tsv'
+test_metadata_path = '../dataset/sv-SE/test.tsv'
+train_metadata = pd.read_csv(train_metadata_path, sep='\t')
+test_metadata = pd.read_csv(test_metadata_path, sep='\t')
+
+train_dataset, vocab = create_dataset(train_metadata, base_dir="../dataset/sv-SE/clips/new/")
+test_dataset, _ = create_dataset(test_metadata, base_dir="../dataset/sv-SE/clips/new/", vocab=vocab)
+test_dataset = test_dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+test_batch = list(test_dataset.take(1))[0] # For training feedback
+
+model = EndToEnd(patch_width, 128)
+
+optimizer = keras.optimizers.Adam(learning_rate=lr)
+loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, label_smoothing=0.1),
+
+# Load checkpoint
+checkpoint_filepath = '/mnt/media/checkpoint/nlp_transformer/'
+checkpoint_cb = checkpoint_cb(checkpoint_filepath)
+
+model.compile(
+    optimizer=optimizer,
+    loss=loss,
+    run_eagerly=True,
+)
+
+model.load_weights(checkpoint_filepath)
+
+max_target_length = 100
+average_cer = 0
+num_batches = len(test_dataset)
+for i, batch in enumerate(test_dataset):
+    source, target = batch
+    target = vocab.decode_docs(target.numpy())
+    pred = model.translate(source, max_target_length, vocab.stoi[vocab.BOS])
+    pred = vocab.decode_docs(pred.numpy())
+    score = cer(target, pred)
+    average_cer += score
+    print(pred[0], target[0], score, average_cer / (i+1))
+
+average_cer /= num_batches
+
+print(average_cer)
+
+
